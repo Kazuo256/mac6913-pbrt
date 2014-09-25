220f259b26e365ec9e2ecbb0a497bd96a02d36ad
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-09-25 11:30:52 -0300

    Implementações dos métodos fáceis XD

--- src/shapes/lsystem.cpp
+++ src/shapes/lsystem.cpp
@@ -3,6 +3,29 @@
 #include "shapes/lsystem.h"
 #include "paramset.h"
 
+#include <numeric>
+
+LSystem::LSystem(const Transform *o2w, const Transform *w2o, bool ro,
+                 const string &description) : Shape(o2w, w2o, ro) {
+
+}
+
+BBox LSystem::ObjectBound() const {
+    if (terminals.empty())
+        return BBox(Point());
+    return std::accumulate(terminals.begin()+1, terminals.end(),
+                           terminals.front()->ObjectBound(),
+                           [](const BBox &lhs, const Reference<Shape> &rhs)
+                               -> BBox {
+                               return Union(lhs, rhs->ObjectBound());
+                           });
+}
+
+void LSystem::Refine(vector<Reference<Shape>> &refined) const {
+    for (auto &shape : terminals)
+        refined.push_back(shape);
+}
+
 LSystem *CreateLSystemShape(const Transform *o2w, const Transform *w2o,
                             bool reverseOrientation, const ParamSet &params) {
     return nullptr;
--- src/shapes/lsystem.h
+++ src/shapes/lsystem.h
@@ -9,17 +9,21 @@
 // shapes/lsystem.h*
 #include "shape.h"
 
+#include <memory>
+
 // LSystem Declarations
 class LSystem : public Shape {
 public:
     // LSystem Public Methods
     LSystem(const Transform *o2w, const Transform *w2o, bool ro,
-            const std::string& description);
+            const string &description);
     BBox ObjectBound() const override;
     bool CanIntersect() const override { return false; }
     void Refine(vector<Reference<Shape>> &refined) const override;
 private:
     // LSystem Private Data
+    vector<Reference<Shape>> terminals;
+    vector<std::unique_ptr<Transform>> transformations;
 };
 
 LSystem *CreateLSystemShape(const Transform *o2w, const Transform *w2o,
